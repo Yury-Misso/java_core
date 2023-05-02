cf424528a45e8b1d8b597cdd4b5967a9b628db4e
Yury-Misso <VectorX69@gmail.com>
2023-05-02 19:59:53 +0300

    Add ParameterizedClass
    learning generics

--- src/comparator_comparable/IComparable.java
+++ src/comparator_comparable/IComparable.java
@@ -2,7 +2,6 @@ package comparator_comparable;
 
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class IComparable {
@@ -13,9 +12,12 @@ public class IComparable {
         carList.add(new Car("Audi", 2021, 2.0));
         carList.add(new Car("Kia", 2019, 1.6));
 
+        System.out.println("\u001B[34m");
         carList.forEach(System.out::println);
         carList.sort(new ComparatorByEngineDisplacement());
+        System.out.println("\u001B[31m");
         carList.forEach(System.out::println);
+        System.out.println("\u001B[0m");
 
     }
 
---
+++ src/genericsLesson/ParameterizedClass.java
@@ -0,0 +1,7 @@
+package genericsLesson;
+
+public class ParameterizedClass {
+    public static void main(String[] args) {
+        
+    }
+}
